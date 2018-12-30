2c29628a76c4830f7c10b8502cfc40533fc07ee7
Vili <vili_mihovska@live.com>
2018-12-31 00:14:38 +0200

    caloriecounter setup

--- caloriecounter/src/index.js
+++ caloriecounter/src/index.js
@@ -1,1 +1,8 @@
-console.log('It Worked!');
+import app from './App';
+import initModel from './Model';
+import view from './View';
+import update from './Update';
+
+const node = document.getElementById('app');
+
+app(initModel, update, view, node);
