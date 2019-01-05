cf705c75ee321813ae6edc3224a1c0437c6f1196
Vili <vili_mihovska@live.com>
2019-01-06 00:02:31 +0200

    add model and view input fields

--- tipcalculator_starter/src/Model.js
+++ tipcalculator_starter/src/Model.js
@@ -1,5 +1,8 @@
 const initModel = {
-  
+  billAmount: 10.00,
+  tipPercent: 15.00,
+  billTip: 1.50,
+  billTotal: 11.50 
 };
 
 export default initModel;
--- tipcalculator_starter/src/View.js
+++ tipcalculator_starter/src/View.js
@@ -6,12 +6,41 @@ const {
   div,
   h1,
   pre,
+  label,
+  input,
+  form
 } = hh(h);
 
+
+function fields(labelText, inputValue) {
+  return div([
+    label({className: 'db mb1'}, labelText),
+    input({
+      className: 'pa2 input-reset ba w-50 mb2',
+      type: 'text',
+      value: inputValue
+    })
+  ]);
+}
+
+function formView(dispatch, model) {
+  const { billAmount, tipPercent } = model;
+
+  return form(
+    {
+      className: 'w-100 mv2'
+    },
+    [
+      fields('Bill Amount', billAmount),
+      fields('Tip %', tipPercent)
+    ]
+  )
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Tip Calculator'),
+    formView(dispatch, model),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
